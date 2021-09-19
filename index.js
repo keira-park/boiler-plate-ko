96f0892b64be7d0d394f82d2c9829bbc34c4303f
keira-park <ynnus411@naver.com>
2021-09-19 10:04:12 +0900

    비밀 정보 보호

--- index.js
+++ index.js
@@ -1,14 +1,39 @@
 const express = require('express') // express 모듈 가져오기
 const app = express() // express function 이용해서 새로운 express app 생성
 const port = 8800 //백 서버
+const bodyParser = require('body-parser');
 
-const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://Keira-park:abcd1234@cluster0.h8jyn.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
+const config = require('./config/key');
+
+const { User } = require('./models/User');
+
+//application/x-www-form-urlencoded 이렇게 된 데이터를 가져올 수 있게 해줌
+app.use(bodyParser.urlencoded({extended: true}));
+
+//application/json 타입 데이터를 가져올 수 있게 해줌 
+app.use(bodyParser.json());
+
+const mongoose = require('mongoose');
+const { urlencoded } = require('body-parser');
+mongoose.connect(config.mongoURI, {
 }).then(() => console.log('MongoDB Connected...')).catch(
     err => console.log(err))
 
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`)) // port 5000에서 시작
+app.post('/register', (req, res) => {
+    // 회원 가입 시 필요한 정보들을 client에서 가져오면
+    // 그것들을 데이터 베이스에 넣어준다.
 
+    const user = new User(req.body)
+
+    user.save((err, doc) => {
+        if (err) return res.json({ success: false, err})
+        return res.status(200).json({
+            success: true
+        })
+    })
+})
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`)) // port 8800에서 시작
